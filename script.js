f334bd0d87426dff6f1957e24014482a3ab59ecf
LeonardoBang <leonardobang.oficial@gmail.com>
2024-09-24 17:50:23 -0400

    Add new functions

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function link_builder() {
     const number = document.getElementById('number').value
+    const open_link = document.getElementById('open_link').checked
     const code_country = document.getElementById('code_country').value
+    const link_content = document.getElementById('link_content')
+    
+    console.log(open_link)
+
     if (Number(number) != NaN) {
-        window.open("https://wa.me/"+code_country+number)
-        return "https://wa.me/"+code_country+number
+        const link = "https://wa.me/"+code_country+number
+        link_content.textContent = link
+        if (open_link) {
+            window.open(link)
+        }
+        return link
     } else {
         return "error"
     }
+}
+
+document.getElementById('btn_copy').addEventListener('click', clipboardCopy);
+async function clipboardCopy() {
+  let text = document.querySelector("#link_content").value;
+  await navigator.clipboard.writeText(text);
 }
